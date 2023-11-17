3de10fe6d168851312f5e72fcc348206310e377c
Ann Gallinger <nanamina@yandex.ru>
2023-11-17 15:39:44 +0300

    full game, but the code not cleaned

--- new_main.py
+++ new_main.py
@@ -2,7 +2,6 @@ import pygame
 from pygame import *
 import sys
 import random
-#import time
 
 pygame.init()
 
@@ -13,6 +12,7 @@ width, height = 500, 500
 BLACK = (0 , 0 , 0)
 WHITE = (255 , 255 , 255)
 GREY = (200, 200, 200)
+RED = (255, 0, 0)
 
 #задам шрифт
 front = font.SysFont('monospace', 36) 
@@ -167,9 +167,106 @@ def average_function():
         pygame.display.update()
         clock.tick(30)  # Ограничение скорости обновления (10 кадров в секунду)
 
+#3 уровень сложный
+def hard_function():
+    root = pygame.display.set_mode((500, 500))
+    pygame.display.set_caption("Game of Life. Hard")
+    cells = [[0 for j in range(root.get_width() // 20)] for i in range(root.get_height() // 20)]
 
+    def near(pos: list, system=[[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]):
+        count = 0
+        for i in system:
+            if cells[(pos[0] + i[0]) % len(cells)][(pos[1] + i[1]) % len(cells[0])]:
+                count += 1
+        return count
 
+    running = True
+    drawing = False
+    autoplay = False
+    draw_counter = 0
+    draw_limit = 25
+    red_cells = []
 
+    clock = pygame.time.Clock()
+
+    while running:
+        root.fill(WHITE)
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
+            elif event.type == MOUSEBUTTONDOWN:
+                drawing = True
+            elif event.type == MOUSEBUTTONUP:
+                drawing = False
+                if draw_counter == draw_limit:
+                    # # Генерация случайных координат для 100 красных клеток
+                    red_cells = set()
+                    while len(red_cells) < 50:
+                        i, j = random.randint(0, len(cells) - 1), random.randint(0, len(cells[0]) - 1)
+                        if cells[i][j] == 0:  # Проверка, не заполнена ли клетка зеленым
+                            red_cells.add((i, j))
+
+                        # Присвоение красным клеткам значение 2
+                    for i, j in red_cells:
+                        cells[i][j] = 2
+
+            elif event.type == KEYDOWN:
+                if event.key == K_RIGHT:
+                    autoplay = not autoplay
+
+        if autoplay:
+            # Обновление клеток
+            cells2 = [[0 for j in range(len(cells[0]))] for i in range(len(cells))]
+            for i in range(len(cells)):
+                for j in range(len(cells[0])):
+                    if cells[i][j]:
+                        # Проверка, что клетка зеленая
+                        if cells[i][j] == 1:
+                            if near([i, j]) not in (2, 3):
+                                cells2[i][j] = 0
+                                continue
+                            # Проверка на наличие красных клеток в окрестности
+                            if any(cells[(i + k[0]) % len(cells)][(j + k[1]) % len(cells[0])] == 2 for k in [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]):
+                                cells2[i][j] = 0  # Зеленая клетка погибает при наличии красной в окрестности
+                                continue
+                            cells2[i][j] = 1
+                            continue
+                        # Проверка, что клетка красная
+                        elif cells[i][j] == 2:
+                            # Проверка на наличие 4 зеленых клеток в окрестности
+                            if near([i, j]) == 4:
+                                cells2[i][j] = 0  # Красная клетка погибает при наличии 4 зеленых в окрестности
+                                continue
+                            cells2[i][j] = 2
+                            continue
+                    if near([i, j]) == 3:
+                        cells2[i][j] = 1
+                        continue
+                    cells2[i][j] = 0
+            cells = cells2
+
+        # Обработка рисования клеток при нажатии мыши
+        if drawing:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            cell_x, cell_y = mouse_x // 20, mouse_y // 20
+            if 0 <= cell_x < len(cells) and 0 <= cell_y < len(cells[0]):
+                if cells[cell_x][cell_y] == 0 and draw_counter < draw_limit:
+                    cells[cell_x][cell_y] = 1
+                    draw_counter += 1
+
+        # Рисование клеток
+        for i in range(len(cells)):
+            for j in range(len(cells[i])):
+                if (i, j) in red_cells:
+                    pygame.draw.rect(root, RED, [i * 20, j * 20, 20, 20])
+                else:
+                    pygame.draw.rect(root, (0, 255 * cells[i][j] % 256, 0), [i * 20, j * 20, 20, 20])
+
+        pygame.display.update()
+        clock.tick(30)
+
+    pygame.quit()
 
 
 # Создание окна меню
@@ -180,7 +277,7 @@ pygame.display.set_caption("Hello, this is the Game of Life. Select difficulty l
 menu = Menu()
 menu.append_option('contemplation', contemplation_function)
 menu.append_option('average', average_function)
-menu.append_option('hard', lambda: print ('hallo3'))
+menu.append_option('hard', hard_function)
 menu.append_option('exit', quit)
 
 running = True
